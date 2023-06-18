3ef36556e067cdb096ed2118764779847dacb3c6
mamun-1995 <127647492+mamun-1995@users.noreply.github.com>
2023-06-18 15:08:47 +0600

    Add files via upload

---
+++ Portfolio of Mamunur Rashid/portfolio/index.js
@@ -0,0 +1,8 @@
+var introEffect= new typed(".intro",{
+strings : ["CODER","WEB DEVELOPER","SOFTWARE DEVELOPER"],
+loop : true,
+typeSpeed : 100,
+backSpeed : 80,
+backDelay: 1500;
+})
+document.querySelector("h1").innerHTML="mamun";
